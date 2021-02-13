7d4441b8488f302366a9315e360443fbc0576429
Andaleeb Khogaly <andaleebkhog@gmail.com>
2021-02-13 06:11:50 +0200

    customized arguments of convert pipe

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -5,13 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: number, ...args: unknown[]): any {
+  transform(value: number, targetedUnits: string): any {
     //console.log(value);
     if(!value) {
       return '';
     }
-    else{
-      return value * 1.60934;
+    switch (targetedUnits){
+      case 'm':
+        return value * 1.60934;
+      case 'km':
+        return value * 1.60934 * 1000;
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000;
+      default:
+        return value * 1.60934;
 
     }
   }
