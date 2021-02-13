5d15a7247c7091c697c4b2e9b92a043cecc883c7
Andaleeb Khogaly <andaleebkhog@gmail.com>
2021-02-13 03:06:04 +0200

    titlecase, date and currency pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   name: any;
+  date: any;
+  currency: any;
 
   onNameChange(value: any){
     this.name = value.target.value;
-    console.log(this.name);
+    //console.log(this.name);
+  }
+
+  onDateChange(value:any){
+    this.date = value.target.value;
+  }
+
+  onCurrencyChange(value:any){
+    this.currency = value.target.value;
   }
 }
